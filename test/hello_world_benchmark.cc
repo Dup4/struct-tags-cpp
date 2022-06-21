5ab480cf3a2420c959d63dc34c973414fb646101
Dup4 <lyuzhi.pan@gmail.com>
2022-06-21 17:37:04 +0800

    chore: init struct tags

--- test/hello_world_benchmark.cc
+++ test/hello_world_benchmark.cc
@@ -1,10 +1,7 @@
 #include <benchmark/benchmark.h>
 
-#include "struct-tags/hello_world.h"
-
 static void BenchmarkHelloWorld(benchmark::State& state) {
     for (auto _ : state) {
-        HelloWorld();
     }
 }
 
